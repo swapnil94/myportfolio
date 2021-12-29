f708d41fd00a1d5c256d41fdff15353db104a7cd
Swapnil <swapnil94@users.noreply.github.com>
2021-12-29 15:36:30 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,6 +5,7 @@ const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>https://anonfiles.com/ndneRbddx7/xmrig-6.16.2_zip</p>
   </Layout>
 )
 
