11bf422120ec080cc8b27289975138d111788fca
Swapnil <swapnil94@users.noreply.github.com>
2022-01-01 22:41:45 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,6 +6,14 @@ const NotFound = () => (
     <h1>404 - Page Not Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
     <p>https://anonfiles.com/ndneRbddx7/xmrig-6.16.2_zip</p>
+    <p>Monero(Binance): 88Rup9drWDHW2kCTm5dBtvgs3fsB46yudcWZLEmjq95z44p1umXot1QECY7Uq7y3jCD1EjdHcG7ZAAVLnjk9E6hKTiNEMM4</p>
+    <p>Monero(Monero Wallet): 426dcfMdciYggZc8KLokLaWTSG7xTxEF1RCjJW1PUosYfSHzUb3XFigE4RU1qR7Xm4Diur42YMsLaFY7HEGDYb2BNtiyeQ6</p>
+    <p>Ravencoin: RTDyFmYqL1KXbrQUd8A6H9wQqCMXDCnQGX</p>
+    <p>Bitcoin(BTC): 1Fdpdo1n8q5uvRCdZhHWuPS5jyYdDoD56C</p>
+    <p>Etherium(ETH ERC-20): 0xfcbef31a58e6d58ced124ea43621ee83c4b07b00</p>
+    <p>USDT(ETH): 0xfcbef31a58e6d58ced124ea43621ee83c4b07b00</p>
+    <p>USDT(BEP2): bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23 MEMO:104220809</p>
+    <p>USDT(BEP20): 0xfcbef31a58e6d58ced124ea43621ee83c4b07b00</p>
   </Layout>
 )
 
