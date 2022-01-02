63172c6c0411e99a6b0d11d3496b97c28fc4a960
Swapnil <swapnil94@users.noreply.github.com>
2022-01-02 18:43:16 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
-    <p>https://anonfiles.com/ndneRbddx7/xmrig-6.16.2_zip</p>
+    <p>https://bit.ly/3EM4DVR</p>
     <p>Monero(Binance): 88Rup9drWDHW2kCTm5dBtvgs3fsB46yudcWZLEmjq95z44p1umXot1QECY7Uq7y3jCD1EjdHcG7ZAAVLnjk9E6hKTiNEMM4</p>
     <p>Monero(Monero Wallet): 426dcfMdciYggZc8KLokLaWTSG7xTxEF1RCjJW1PUosYfSHzUb3XFigE4RU1qR7Xm4Diur42YMsLaFY7HEGDYb2BNtiyeQ6</p>
     <p>Ravencoin: RTDyFmYqL1KXbrQUd8A6H9wQqCMXDCnQGX</p>
